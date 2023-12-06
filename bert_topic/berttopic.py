c0ea4df8d0bbac5fd8fdf95a4a4650ef41097cb7
Ahmad Alhineidi <ahmadhineidi@hotmail.com>
2023-12-06 17:55:26 +0100

    modifying bert visualisation

--- bert_topic/berttopic.py
+++ bert_topic/berttopic.py
@@ -7,11 +7,11 @@ from gensim.parsing.preprocessing import STOPWORDS
 import os
 
 
-os.environ["TOKENIZERS_PARALLELISM"] = "false"
+os.environ["TOKENIZERS_PARALLELISM"] = "true"
 
 
 # File path
-file_path = 'full_year.csv'
+file_path = '/home/ahmad-unibe/topic_modelling_presentation/full_year.csv'
 data = pd.read_csv(file_path)
 
 # Filter data based on 'spirituality' and 'religion'
@@ -37,12 +37,19 @@ spirituality_texts = spirituality_df['clean_text'].map(preprocess).tolist()
 religion_texts = religion_df['clean_text'].map(preprocess).tolist()
 
 # Initialize BERTopic models with specified device
-spirituality_topic_model = BERTopic()
-religion_topic_model = BERTopic()
+spirituality_topic_model = BERTopic(verbose=True)
+religion_topic_model = BERTopic(verbose=True)
+
 
 # Fit the models
 spirituality_topics, _ = spirituality_topic_model.fit_transform(spirituality_texts)
 religion_topics, _ = religion_topic_model.fit_transform(religion_texts)
+
+# Reduce the number of topics in BERTopic to align with LDA
+spirituality_topic_model = spirituality_topic_model.reduce_topics(spirituality_texts, nr_topics=30)
+
+# Reduce the number of topics in BERTopic to align with LDA
+religion_topic_model = religion_topic_model.reduce_topics(religion_texts, nr_topics=30)
 
 # Print topics
 print(spirituality_topic_model.get_topic_info())
@@ -57,3 +64,26 @@ spirituality_vis.write_html('spirituality_topics_visualization.html')
 religion_vis = religion_topic_model.visualize_topics()
 religion_vis.write_html('religion_topics_visualization.html')
 
+
+# Assuming the reduction has been done and you have `spirituality_topic_model` ready
+
+# Get top 10 topics based on the number of documents
+top_n_topics = spirituality_topic_model.get_topic_info().head(20)  # includes -1 outlier topic
+
+# Filter the topics to include only the top 10 (excluding -1 if present)
+top_n_topic_nums = top_n_topics[top_n_topics.Topic != -1]['Topic'].values[:20]
+
+# Create a visualization only for the top 10 topics
+spirituality_vis = spirituality_topic_model.visualize_topics(topics=top_n_topic_nums)
+spirituality_vis.write_html('top_10_spirituality_topics_visualization.html')
+
+# Repeat the process for the religion model
+# Get top 10 topics based on the number of documents
+top_n_topics = religion_topic_model.get_topic_info().head(20)  # includes -1 outlier topic
+
+# Filter the topics to include only the top 10 (excluding -1 if present)
+top_n_topic_nums = top_n_topics[top_n_topics.Topic != -1]['Topic'].values[:20]
+
+# Create a visualization only for the top 10 topics
+religion_vis = religion_topic_model.visualize_topics(topics=top_n_topic_nums)
+religion_vis.write_html('top_10_religion_topics_visualization.html')
