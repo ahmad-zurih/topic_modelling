fe6507e30df54667dd93fdcb35205c618f6010c6
Ahmad Alhineidi <ahmadhineidi@hotmail.com>
2023-12-06 20:33:36 +0100

    add similarity output

--- compare_lda_bert.py
+++ compare_lda_bert.py
@@ -51,79 +51,25 @@ def jaccard_similarity(set1, set2):
     union = len(set(set1).union(set2))
     return intersection / union if union != 0 else 0
 
-# Compare Topics and Print Common Words
+# Compare Topics and Print Common and Uncommon Words
 def compare_topics(lda_topics, bertopic_topics):
     similarity_matrix = []
     for lda_idx, lda_topic in lda_topics.items():
         for bertopic_idx, bertopic_topic in bertopic_topics.items():
             similarity = jaccard_similarity(lda_topic, bertopic_topic)
             common_words = set(lda_topic).intersection(bertopic_topic)
-            similarity_matrix.append((lda_idx, bertopic_idx, similarity, common_words))
+            uncommon_words_lda = set(lda_topic).difference(bertopic_topic)
+            uncommon_words_bertopic = set(bertopic_topic).difference(lda_topic)
+            similarity_matrix.append((lda_idx, bertopic_idx, similarity, common_words, uncommon_words_lda, uncommon_words_bertopic))
     return sorted(similarity_matrix, key=lambda x: x[2], reverse=True)
 
-# Run comparison and report the topics with common words
+# Run comparison and report the topics with common and uncommon words
 topic_similarities = compare_topics(lda_topics, bertopic_topics)
-for lda_idx, bertopic_idx, sim, common_words in topic_similarities:
+for lda_idx, bertopic_idx, sim, common_words, uncommon_words_lda, uncommon_words_bertopic in topic_similarities:
     print(f"LDA Topic {lda_idx} and BERTopic Topic {bertopic_idx} have a similarity score of: {sim}")
-    print(f"Common Words: {', '.join(common_words)}\n")
-
-
-
-# Filter out topic pairs with zero similarity
-filtered_topic_similarities = [item for item in topic_similarities if item[2] > 0]
-
-# Extract data for the bar chart
-lda_indices, bertopic_indices, similarities, common_words_list = zip(*filtered_topic_similarities)
-unique_lda_indices = sorted(set(lda_indices))
-bar_width = 0.35  # the width of the bars
-
-# Create a bar chart
-fig, ax = plt.subplots(figsize=(15, 7))
-
-# Generate bar positions
-lda_bar_positions = list(range(len(unique_lda_indices)))
-bertopic_bar_positions = [x + bar_width for x in lda_bar_positions]
-
-# Gather the highest similarity score for each unique LDA topic
-lda_bars = []
-bertopic_bars = []
-for lda_idx in unique_lda_indices:
-    lda_similarities = [item[2] for item in filtered_topic_similarities if item[0] == lda_idx]
-    bertopic_similarities = [item[2] for item in filtered_topic_similarities if item[1] == lda_idx]
-    # Take the max similarity for each LDA topic, if there are multiple
-    lda_bars.append(max(lda_similarities) if lda_similarities else 0)
-    bertopic_bars.append(max(bertopic_similarities) if bertopic_similarities else 0)
-
-# Plot each set of bars
-lda_bars_plot = ax.bar(lda_bar_positions, lda_bars, bar_width, label='LDA')
-bertopic_bars_plot = ax.bar(bertopic_bar_positions, bertopic_bars, bar_width, label='BERTopic')
-
-# Add some text for labels, title, and axes ticks
-ax.set_xlabel('Topic Index')
-ax.set_ylabel('Similarity Score')
-ax.set_title('Topic Similarity Scores between LDA and BERTopic')
-ax.set_xticks([index + bar_width / 2 for index in range(len(unique_lda_indices))])
-ax.set_xticklabels(unique_lda_indices)
-ax.legend()
-
-# Function to add a label above each bar
-def add_labels(bars):
-    for bar in bars:
-        height = bar.get_height()
-        if height > 0:  # Only label bars with a height > 0 (i.e., similarity > 0)
-            ax.annotate('{}'.format(round(height, 2)),
-                        xy=(bar.get_x() + bar.get_width() / 2, height),
-                        xytext=(0, 3),  # 3 points vertical offset
-                        textcoords="offset points",
-                        ha='center', va='bottom', fontsize=8)
-
-# Call the function to add labels
-add_labels(lda_bars_plot)
-add_labels(bertopic_bars_plot)
-
-# Show the plot
-plt.tight_layout()
-plt.show()
+    print(f"Common Words: {', '.join(common_words)}")
+    print(f"Uncommon Words in LDA: {', '.join(uncommon_words_lda)}")
+    print(f"Uncommon Words in BERTopic: {', '.join(uncommon_words_bertopic)}\n")
 
 
 # Cleanup
